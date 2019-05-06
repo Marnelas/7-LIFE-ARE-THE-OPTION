fa6b0445bd868cbfcb8750d3cc0909db38d85127
Marnelas <marnelasbenito@gmail.com>
2019-05-06 11:40:27 +0200

    new files and changes

---
+++ js/background.js
@@ -0,0 +1,20 @@
+//esta función mantiene el fondo del juego
+class Background {
+  constructor(w, h, ctx) {
+    this.ctx = ctx;
+    this.img = new Image();
+    this.img.src = "img/bg.png";
+    this.h = h;
+    this.w = w;
+
+    this.x = 0;
+    this.y = 0;
+
+    this.dx = 10;
+  }
+
+  draw() {
+    this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+    this.ctx.drawImage(this.img, this.x + this.w, this.y, this.w, this.h);
+  }
+}
---
+++ js/firstScreen.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+  baseGame.init("nivel1");
+};

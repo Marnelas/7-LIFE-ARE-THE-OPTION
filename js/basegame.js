d80ad30c77fc8721d227f34be0954fc746d6f009
Marnelas <marnelasbenito@gmail.com>
2019-05-10 09:53:37 +0200

    winScreen and loseScreen updates

--- js/basegame.js
+++ js/basegame.js
@@ -2,11 +2,11 @@ let basegame = {
   canvas: undefined,
   ctx: undefined,
   fps: 60,
-sound: undefined,
-counterSong:0,
-winSound:undefined,
+  sound: undefined,
+  counterSong: 0,
+  winSound: undefined,
   winH: undefined,
-  counter:0,
+  counter: 0,
   winW: undefined,
   init: function(Id) {
     this.canvas = document.getElementById(Id);
@@ -21,9 +21,8 @@ winSound:undefined,
     this.canvas.setAttribute("width", window.innerWidth);
     this.canvas.setAttribute("height", window.innerHeight);
     this.winH = window.innerHeight;
-    this.winW = window.innerWidth; 
+    this.winW = window.innerWidth;
     this.reset();
-
   },
   setHandlers: function() {
     window.onresize = () => this.setDimensions();
@@ -34,7 +33,7 @@ winSound:undefined,
     this.reset();
 
     this.interval = setInterval(() => {
-      this.framesCounter++
+      this.framesCounter++;
       this.clear();
       if (this.player.contador == 15) this.gameOver();
       this.player.gravity();
@@ -51,36 +50,33 @@ winSound:undefined,
     this.createObstacle(new Obstacle(this.ctx, this.winW, this.winH, 400, 450));
     this.createObstacle(new Obstacle(this.ctx, this.winW, this.winH, 600, 350));
     this.createObstacle(new Obstacle(this.ctx, this.winW, this.winH, 800, 250));
-    this.createObstacle(new Obstacle(this.ctx, this.winW, this.winH, 1000, 150));
-    this.createObstacle(new Obstacle(this.ctx, this.winW, this.winH, 1200, 150));
- 
- 
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 800, 450))    
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 600, 450))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 400, 550))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 300, 600))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 500, 50))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 700, 20))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1200, 550))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 200, 250))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1000, 450))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1200, 350))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1100, 250))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 900, 300))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 700, 450))
-    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 500, 500))
+    this.createObstacle(
+      new Obstacle(this.ctx, this.winW, this.winH, 1000, 150)
+    );
+    this.createObstacle(
+      new Obstacle(this.ctx, this.winW, this.winH, 1200, 150)
+    );
 
-
-
-
-
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 800, 450));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 600, 450));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 400, 550));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 300, 600));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 500, 50));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 700, 20));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1200, 550));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 200, 250));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1000, 450));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1200, 350));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 1100, 250));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 900, 300));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 700, 450));
+    this.createTrap(new Obstacle(this.ctx, this.winW, this.winH, 500, 500));
 
     this.framesCounter = 0;
   },
-  
-  createTrap: function(trap){
-    this.traps.push(trap);
 
+  createTrap: function(trap) {
+    this.traps.push(trap);
   },
   createObstacle: function(obs) {
     this.obstacles.push(obs);
@@ -91,16 +87,15 @@ winSound:undefined,
       obstacle.draw();
     });
     this.traps.forEach(trap => {
-      trap.draw()
+      trap.draw();
     });
     this.player.draw(this.framesCounter);
-    },
+  },
   clear: function() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
   setEventListeners: function() {
     document.onkeydown = e => {
-
       if (e.keyCode === 37) this.player.moveLeft();
       if (e.keyCode === 39) this.player.moveRight();
       if (e.keyCode === 32) this.player.jump();
@@ -110,29 +105,27 @@ winSound:undefined,
     clearInterval(this.interval);
   },
   gameOver: function() {
-    this.gameOverSound()
+    this.gameOverSound();
     this.stop();
-    let lose = document.getElementById("loseScreen")
+    let lose = document.getElementById("loseScreen");
 
-    lose.classList.remove("bloqueo")
-    let canvas = document.getElementById("nivel")
-    canvas.className = "bloqueo"
+    lose.classList.remove("bloqueo");
+    let canvas = document.getElementById("nivel");
+    canvas.className = "bloqueo";
   },
   colission: function() {
     let index = this.obstacles.findIndex(obstacle => {
       return (
-           this.player.x <= obstacle.posX + obstacle.width - 25 &&
-           this.player.x + this.player.w - 50 >= obstacle.posX &&
-          this.player.y <= obstacle.posY - 30 &&  
+        this.player.x <= obstacle.posX + obstacle.width - 25 &&
+        this.player.x + this.player.w - 50 >= obstacle.posX &&
+        this.player.y <= obstacle.posY - 30 &&
         this.player.y + this.player.h >= obstacle.posY
       );
     });
     if (index != -1) {
       if (index == 0 && this.counterSong == 0) {
-
-
-        this.background.Sound()
-        this.counterSong++
+        this.background.Sound();
+        this.counterSong++;
       }
 
       this.winCondition(index);
@@ -148,65 +141,56 @@ winSound:undefined,
         this.player.y + this.player.h >= trap.posY
       );
     });
-    if(condition)this.goBack()
-  
+    if (condition) this.goBack();
   },
   colissionAction: function(i) {
     this.player.positionF = this.obstacles[i].posY - this.player.h;
     this.player.floor = true;
     console.log("entra");
-    if (i == 3 && this.counter==0) {
-    this.endComming()
-    this.counter++
-  
+    if (i == 3 && this.counter == 0) {
+      this.endComming();
+      this.counter++;
     }
-    
   },
   goBack: function() {
- /*colissionSound: undefined,
+    /*colissionSound: undefined,
   gameOverSound:undefined,
   endComming:undefined,*/
-  this.colissionSound()
-    this.player.x = this.winW * 0.08
+    this.colissionSound();
+    this.player.x = this.winW * 0.08;
     this.player.y = this.player.y0;
   },
-  colissionSound: function(){
-    this.player.contador = 0
+  colissionSound: function() {
+    this.player.contador = 0;
     this.sound = new Audio();
-    this.sound.src = "songs/desvanecido.wav"
-    this.sound.play()
+    this.sound.src = "songs/desvanecido.wav";
+    this.sound.play();
   },
-  gameOverSound:function(){
+  gameOverSound: function() {
     this.sound = new Audio();
-    this.sound.src = "songs/explotido.wav"
-    this.sound.play()
+    this.sound.src = "songs/explotido.wav";
+    this.sound.play();
   },
-  endComming:function(){
+  endComming: function() {
     this.sound = new Audio();
-    this.sound.src = "songs/Llegando-fin.wav"
-    this.sound.play()
-
+    this.sound.src = "songs/Llegando-fin.wav";
+    this.sound.play();
   },
 
   winCondition: function(index) {
     if (index == 5) {
       this.stop();
-      this.winPlay()
-      let win = document.getElementById("winScreen")
-      console.log(win)
-      win.classList.remove("bloqueo")
-      let canvas = document.getElementById("nivel")
-      canvas.className = "bloqueo"
-
-
-      
-    
+      this.winPlay();
+      let win = document.getElementById("winScreen");
+      console.log(win);
+      win.classList.remove("bloqueo");
+      let canvas = document.getElementById("nivel");
+      canvas.className = "bloqueo";
     }
   },
-  winPlay:function(){
-    this.winSound = new Audio()
-    this.winSound.src = "songs/winSong.mp3"
-    this.winSound.play()
+  winPlay: function() {
+    this.winSound = new Audio();
+    this.winSound.src = "songs/winSong.mp3";
+    this.winSound.play();
   }
-  
 };
